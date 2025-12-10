2029d56dfa086479c67a67939113fae7f932c65f
Cristian Palencia <cristian.palencia0211@gmail.com>
2025-12-10 16:44:25 -0500

    added Card class

---
+++ backend/app/game/cards.py
@@ -0,0 +1,12 @@
+'''
+Enum class for the different types of cards in the game.
+'''
+
+from enum import Enum
+
+class Card(Enum):
+    DUKE = "Duke"
+    ASSASSIN = "Assassin"
+    CAPTAIN = "Captain"
+    AMBASSADOR = "Ambassador"
+    CONTESSA = "Contessa"
